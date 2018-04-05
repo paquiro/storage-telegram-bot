49aee74142a6e22757bb9280f6563f21af367c88
Iván Portillo <ivanportillo@me.com>
2018-04-06 00:45:31 +0200

    Refactor index.js with new controllers and services

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const request = require("request");
 const fs = require("fs");
 const Telegraf = require("telegraf");
 const Markup = require("telegraf/markup");
-const Extra = require("telegraf/extra");
 const config = require("config");
 
-const auth = require("./auth");
+const doorman = require("./services/doorman");
+const controllers = require("./controllers");
 
 if (!config.telegram.token) {
   console.log("Please enter a token");
@@ -17,40 +17,10 @@ if (!config.telegram.token) {
 const bot = new Telegraf(config.telegram.token);
 
 // Authorization
-bot.use((ctx, next) => {
-  const userId = ctx.message.from.id;
+bot.use(doorman);
 
-  if (auth.isUserAllowed(userId)) {
-    return next();
-  }
-  return ctx.reply("You are not allowed to insert data on this Cloud");
-});
+bot.on("photo", controllers.receivePhoto);
 
-bot.on("photo", ctx => {
-  ctx.reply(
-    "If you want to storage the image, send it like a document.",
-    Extra.inReplyTo(ctx.message.message_id)
-  );
-});
-
-bot.on("document", ctx => {
-  const document = ctx.message.document;
-
-  bot.telegram.getFile(document.file_id).then(file => {
-    request
-      .get(
-        `https://api.telegram.org/file/botd${config.telegram.token}/${
-          file.file_path
-        }`
-      )
-      .on("response", response => {
-        ctx.reply("File saved successfully");
-      })
-      .pipe(
-        // TODO Add ${path} between basePath and name
-        fs.createWriteStream(`${process.env.basePath}${document.file_name}`)
-      );
-  });
-});
+bot.on("document", controllers.receiveDocument);
 
 bot.startPolling();
