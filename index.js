5fea6582fdd6446e8efedb3292499896a9220555
Iván Portillo <ivanportillo@me.com>
2018-04-10 23:02:28 +0200

    Fix eslint/prettier errors in index file

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-"use strict";
+const request = require('request');
+const fs = require('fs');
+const Telegraf = require('telegraf');
+const Extra = require('telegraf/extra');
+const config = require('config');
 
-const request = require("request");
-const fs = require("fs");
-const Telegraf = require("telegraf");
-const Markup = require("telegraf/markup");
-const Extra = require("telegraf/extra");
-const config = require("config");
-
-const auth = require("./auth");
+const auth = require('./auth');
 
 if (!config.telegram.token) {
-  console.log("Please enter a token");
+  console.log('Please enter a token');
   process.exit(0);
 }
 
@@ -23,32 +20,32 @@ bot.use((ctx, next) => {
   if (auth.isUserAllowed(userId)) {
     return next();
   }
-  return ctx.reply("You are not allowed to insert data on this Cloud");
+  return ctx.reply('You are not allowed to insert data on this Cloud');
 });
 
-bot.on("photo", ctx => {
+bot.on('photo', ctx => {
   ctx.reply(
-    "If you want to storage the image, send it like a document.",
-    Extra.inReplyTo(ctx.message.message_id)
+    'If you want to storage the image, send it like a document.',
+    Extra.inReplyTo(ctx.message.message_id),
   );
 });
 
-bot.on("document", ctx => {
-  const document = ctx.message.document;
+bot.on('document', ctx => {
+  const { document: { document } } = ctx.message;
 
   bot.telegram.getFile(document.file_id).then(file => {
     request
       .get(
         `https://api.telegram.org/file/botd${config.telegram.token}/${
           file.file_path
-        }`
+        }`,
       )
-      .on("response", response => {
-        ctx.reply("File saved successfully");
+      .on('response', () => {
+        ctx.reply('File saved successfully');
       })
       .pipe(
         // TODO Add ${path} between basePath and name
-        fs.createWriteStream(`${process.env.basePath}${document.file_name}`)
+        fs.createWriteStream(`${process.env.basePath}${document.file_name}`),
       );
   });
 });
